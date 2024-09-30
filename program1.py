2f09b3cb97a27652849c876c3309b74d58f89b49
nilesh-1980 <120701720+nilesh-1980@users.noreply.github.com>
2024-09-30 14:38:14 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,25 +1,25 @@
 class Solution(object):
     def isValid(s: str) -> bool:
-    # Stack to store opening brackets
-    stack = []
-    # Dictionary to map closing to opening brackets
-    bracket_map = {')': '(', '}': '{', ']': '['}
-
-    # Loop through each character in the string
-    for char in s:
-        # If it's a closing bracket, check for a match in the stack
-        if char in bracket_map:
-            # Pop the top of the stack if there's something, otherwise use a dummy value '#'
-            top_element = stack.pop() if stack else '#'
-            # If the top of the stack doesn't match the corresponding opening bracket
-            if bracket_map[char] != top_element:
-                return False
-        else:
-            # If it's an opening bracket, push it to the stack
-            stack.append(char)
-
-    # If the stack is empty, it means all brackets were properly closed
-    return not stack
+        # Stack to store opening brackets
+        stack = []
+        # Dictionary to map closing to opening brackets
+        bracket_map = {')': '(', '}': '{', ']': '['}
+    
+        # Loop through each character in the string
+        for char in s:
+            # If it's a closing bracket, check for a match in the stack
+            if char in bracket_map:
+                # Pop the top of the stack if there's something, otherwise use a dummy value '#'
+                top_element = stack.pop() if stack else '#'
+                # If the top of the stack doesn't match the corresponding opening bracket
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                # If it's an opening bracket, push it to the stack
+                stack.append(char)
+    
+        # If the stack is empty, it means all brackets were properly closed
+        return not stack
 
 
 
